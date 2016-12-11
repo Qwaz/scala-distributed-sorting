c028a934be2b7b57ae53f1d6ac79d62abcbbba18
Qwaz <qwazpia@gmail.com>
2016-12-11 21:34:11 +0900

    refactor entry point scripts

--- src/main/scala/dsorting/entrypoint/MasterApp.scala
+++ src/main/scala/dsorting/entrypoint/MasterApp.scala
@@ -9,13 +9,16 @@ import scala.concurrent.{Await, Promise}
 
 object MasterApp extends App {
   val p = Promise[Unit]()
-  ArgumentParser.parseArgument(args) ->
-    SamplingInitializer.prepareSampling flatMap {
-    samplingState => samplingState.run() ->
-      ShufflingInitializer.prepareShuffling(samplingState) flatMap {
-      shufflingState => shufflingState.run() ->
-        { _ => p.success(()) }
-    }
-  } onFailure { case e => p.tryFailure(e) }
+
+  val samplingState =
+    ArgumentParser.parseArgument(args) -> SamplingInitializer.prepareSampling
+  val shufflingState = samplingState flatMap {
+    state => state.run() -> ShufflingInitializer.prepareShuffling(state)
+  }
+  val result = shufflingState flatMap {
+    state => state.run() -> { _ => p.success(()) }
+  }
+  result onFailure { case e => p.tryFailure(e) }
+
   Await.result(p.future, Duration.Inf)
 }
